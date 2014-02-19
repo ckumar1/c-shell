9fe2ce7903de8713c700f389dfd58d15cadbb895
ckumar1 <cherub.kumar@gmail.com>
2014-02-19 01:09:17 -0600

    Fix Max Input validation and some error handling

--- shell.c
+++ shell.c
@@ -10,55 +10,82 @@
 #define STDERR_FILENO 2
 
 #define MAX_INPUT_LENGTH 512 //Max input 512 bytes
-
 /* Functions */
-
 
 int main(int argc, char *argv[]) {
 
-	if (argc == 1) {  //if no arguments are specified run in interactive mode
+	if (argc == 1) {  // if no arguments are specified run in Interactive mode
 
 		// Interactive loop to keep asking user for input
-
-		// Repeated until the user types "exit"
-
 		while (TRUE) {
 
 			// Input variables
 
-
-			//string to store User input (max size is 512 bytes) + null char + return char
-			char input[MAX_INPUT_LENGTH+2];
+			char inputBuf[MAX_INPUT_LENGTH + 2]; // input buffer (max size + '\n' char + '\0' char)
+			size_t buflen; // Number of characters read into buffer not inc null term.
 
 			// prints a prompt
 			printf("mysh> ");
 
 			// Get a newline terminated string from stdin
-			// fgets accepts a stream argument,
-			// also allows to specify the maximum size of input (read till size-1)
-			// includes in the string any ending newline character.
+			// check to make sure fgets returns valid input
+			if (fgets(inputBuf, (MAX_INPUT_LENGTH + 2), stdin)) {
 
-			if(fgets(input, (MAX_INPUT_LENGTH+2), stdin)){
+				buflen = strlen(inputBuf);	// buflen is the number of chars read
 
-			// TESTING print out input (what user just entered)
-			// NOTE input needs a null terminating character saved for printf to work (Check size of input)
-			printf("\ntest:: %s\n", input);
+				if (buflen) {	// if buflen > 0
 
-			// Parses command input
-			// strtok()
+					// Check if input ends with '\n' to validate command size
+					if (inputBuf[buflen - 1] == '\n') {
+
+						puts("no more data (3.1. or 3.2.)"); /* normal situation */
+						// Acceptable input
+						// Parse command from buffer
+
+						// TODO Parses command inputBuf
+						// TODO executes the command specified on that line of inputBuf,
+						// TODO waits for the command to finish.
+						// TODO Repeated until the user types "exit"a
+
+					} else { //Line does not terminate with '\n'
+
+						if (buflen + 1 == sizeof inputBuf) { /* If the buffer is full case: long input line */
 
 
-			// Runs the appropriate type of command
+//			                puts("more data waiting (3.3.)\n"); /* long input line */
 
-			// TODO executes the command specified on that line of input,
-			// TODO waits for the command to finish.
-			}
-			else {
-			    if (feof(stdin)) {
-			        puts("EOF reached (2.)"); /* normal situation */
-			    } else {
-			        puts("error in input (1.)");
-			    }
+							// Display Error
+							char error_message[30] = "An error has occurred\n";
+							write(STDERR_FILENO, error_message, strlen(error_message));
+
+							// Flush stdin
+							int c;
+							while ((c = getchar()) != '\n' && c != EOF);
+
+							// Start from the top of the while loop
+							continue;
+
+
+
+						} else { // EOF reached before line break
+							puts("EOF reached before line break (3.1.)"); /* shouldn't happen */
+						}
+					}
+				} else {
+					puts("EOF reached before line break (3.1.)"); /* shouldn't happen */
+				}
+
+
+			} else {
+				if (feof(stdin)) { // unlikely to reach in interactive mode
+
+					// puts("EOF reached (2.)"); // TEST output*/
+					//Exit cleanly
+					exit(0);
+
+				} else {
+					puts("error in inputBuf (1.)");
+				}
 			}
 		}
 
@@ -66,7 +93,6 @@ int main(int argc, char *argv[]) {
 
 		// check if batch file is valid by opening
 		// fopen()
-
 
 		// get line of input from file
 		// fgets()
@@ -85,3 +111,14 @@ int main(int argc, char *argv[]) {
 	}
 }
 
+/* function dump_line
+*  This function reads and dumps any remaining characters on the current input
+*  line of a file.
+*  Parameter:
+*     fp - pointer to a FILE to read characters from
+*  Precondition:
+*     fp points to a open file
+*  Postcondition:
+*     the file referenced by fp is positioned at the end of the next line
+*     or the end of the file.
+//*/
